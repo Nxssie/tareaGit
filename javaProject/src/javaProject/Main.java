691d07ca045d0150dc5e561d2178e5cf4aed0593
Carlos <nxssiedev@gmail.com>
2020-03-22 16:56:25 +0000

    added Java project

---
+++ javaProject/src/javaProject/Main.java
@@ -0,0 +1,23 @@
+package javaProject;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.println("Set the value of n to do the addition of the n firsts numbers.");
+        int n = keyboard.nextInt();
+
+        int suma = 0;
+
+        for (int i = 0; i < n; i++) {
+            suma += i;
+        }
+
+        System.out.println(suma);
+
+    }
+}
